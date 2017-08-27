036d723a011cf3b10f75191e6c107e2f452caf83
Masaaki Kakimoto <karamage.umauma@gmail.com>
2017-08-27 19:51:52 +0900

    first commit

---
+++ app/src/main/kotlin/com/karamage/androidlayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.karamage.androidlayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.bottom_button_layout1)
+    }
+}
