7cd032815b774f3dc05173b453b2ebda33edc377
Masaaki Kakimoto <karamage.umauma@gmail.com>
2017-08-30 08:06:26 +0900

    includeレイアウト

--- app/src/main/kotlin/com/karamage/androidlayout/MainActivity.kt
+++ app/src/main/kotlin/com/karamage/androidlayout/MainActivity.kt
@@ -2,11 +2,25 @@ package com.karamage.androidlayout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.bottom_button_layout2)
+
+        //main2.xml
+        setContentView(R.layout.main2)
+
+        val groupIds = arrayOf(R.id.group1, R.id.group2, R.id.group3,
+                R.id.group4, R.id.group5, R.id.group6)
+
+        groupIds.forEachIndexed { i, gId ->
+            val ll = findViewById(gId) as LinearLayout
+            val button = ll.findViewById(R.id.button)
+            button.setOnClickListener( { v ->
+                val ll = v.parent as LinearLayout
+            })
+        }
     }
 }
